d06e983d773f9801015fcdaeeb3fe15a2b0af1fd
Rajat Gupta <rajat.gupta@openlabs.co.in>
2015-01-30 12:42:57 +0530

    Added source code and HTML Forms with Bootstrap

--- calc.py
+++ calc.py
@@ -14,13 +14,13 @@ def result():
     var_1 = request.form.get("var_1", type=int)
     var_2 = request.form.get("var_2", type=int)
     operation = request.form.get("operation")
-    if(operation == '+'):
+    if(operation == 'Addition'):
         result = var_1 + var_2
-    elif(operation == '-'):
+    elif(operation == 'Subtraction'):
         result = var_1 - var_2
-    elif(operation == '*'):
+    elif(operation == 'Multiplication'):
         result = var_1 * var_2
-    elif(operation == '/'):
+    elif(operation == 'Division'):
         result = var_1 / var_2
     else:
         result = 'INVALID CHOICE'
